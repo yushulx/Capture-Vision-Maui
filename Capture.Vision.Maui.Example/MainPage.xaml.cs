f4ec7f3e3ed4fcc6dce9bd3a07ce286370fc418f
yushulx <lingxiao1002@gmail.com>
2023-06-19 10:31:20 +0800

    Updated README.md

--- Capture.Vision.Maui.Example/MainPage.xaml.cs
+++ Capture.Vision.Maui.Example/MainPage.xaml.cs
@@ -1,10 +1,23 @@
-﻿namespace Capture.Vision.Maui.Example
+﻿using Dynamsoft;
+
+namespace Capture.Vision.Maui.Example
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+            InitService();
+        }
+
+        private async void InitService()
+        {
+            await Task.Run(() =>
+            {
+                BarcodeQRCodeReader.InitLicense("DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==");
+
+                return Task.CompletedTask;
+            });
         }
 
         async void OnTakeVideoButtonClicked(object sender, EventArgs e)
